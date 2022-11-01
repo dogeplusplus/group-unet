117a326e5d2f7b84587549ae28df012e5c17cd93
Albert Chung <albertswchung@gmail.com>
2022-11-01 20:39:06 +0000

    Add function to evaluate rotation equivariance.

--- inference.py
+++ inference.py
@@ -6,9 +6,10 @@ import imageio
 import numpy as np
 import torch.nn as nn
 import albumentations as A
+import matplotlib.pyplot as plt
 import torch.nn.functional as F
 
-from typing import Tuple
+from typing import Tuple, List
 from pathlib import Path
 from einops import repeat
 from scipy.ndimage import rotate
@@ -61,12 +62,27 @@ def load_model(model_uri: str) -> nn.Module:
 
 def batched_prediction(
     model: nn.Module,
-    images: torch.Tensor,
-    batch_size: int,
+    image: np.ndarray,
+    angles: List[float],
 ) -> torch.Tensor:
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    images = []
+    for angle in angles:
+        images.append(rotate(image, angle, reshape=False))
+
+    preprocessing = A.Compose([
+        A.Normalize(),
+        ToTensorV2(),
+    ])
+    images = np.stack([preprocessing(image=image)["image"] for image in images])
+    images = torch.from_numpy(images)
+
+    batch_size = 4
     predictions = []
     for i in range(0, images.shape[0], batch_size):
         pred = torch.sigmoid(model(images[i:i+batch_size])) > 0.5
+        pred = torch.squeeze(pred)
         predictions.append(pred)
 
     predictions = torch.concat(predictions, dim=0)
@@ -88,9 +104,67 @@ def overlay(
     return image_combined
 
 
+def create_gif(
+    image: torch.Tensor,
+    predictions: torch.Tensor,
+    angles: List[float],
+    dest: Path
+):
+    predictions = [x.numpy()[0].astype(np.uint8) * 255 for x in predictions]
+    predictions = [rotate(pred, -angle, reshape=False) for pred, angle in zip(predictions, angles)]
+    predictions = [overlay(image, pred) for pred in predictions]
+
+    frames = []
+    for angle, pred in zip(angles, predictions):
+        frame = Image.fromarray(pred)
+        draw = ImageDraw.Draw(frame)
+        draw.text((0, 0), f"Angle: {angle}", (255, 255, 255))
+        frames.append(frame)
+
+    imageio.mimsave(dest, frames, fps=8)
+
+
+def dice_score(ground_truth: np.ndarray, prediction: np.ndarray) -> float:
+    intersection = np.sum(np.equal(ground_truth, prediction))
+    union = np.sum(ground_truth) + np.sum(prediction)
+    return 2 * intersection / union
+
+
+def evaluate_rotation_equivariance(model: nn.Module, image: np.ndarray, ground_truth: np.ndarray):
+    num_angles = 30
+    angles = np.linspace(0, 360, num_angles)
+    predictions = batched_prediction(model, image, angles).numpy()
+
+    ground_truths = np.stack([
+        rotate(ground_truth, angle, reshape=False) for angle in angles
+    ])
+
+    dices = [dice_score(gt, pred) for gt, pred in zip(ground_truths, predictions)]
+    fig, ax = plt.subplots(2, 2)
+    fig.suptitle("Rotation Equivariance Performance")
+    ax[0, 0].set_title("Image")
+    ax[0, 0].imshow(image)
+    ax[0, 1].set_title("Ground Truth")
+    ax[0, 1].matshow(ground_truth)
+
+    ax[1, 0].plot(angles, dices)
+    ax[1, 1].hist(dices)
+    ax[1, 0].set_xlabel("Rotation Angle")
+    ax[1, 0].set_ylabel("Dice Score")
+
+    ax[1, 1].set_xlabel("Dice Score")
+    ax[1, 1].set_ylabel("Density")
+
+    plt.show()
+
+
 def main():
     images = list(Path("data", "leedsbutterfly_resized", "images").rglob("*.png"))
-    image = str(random.choice(images))
+    image_path = str(random.choice(images))
+    gt_path = image_path.replace("images", "segmentations").replace(".png", "_seg0.png")
+
+    image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+    gt = cv2.imread(gt_path, cv2.IMREAD_GRAYSCALE) / 255
 
     model_path = Path("artifacts", "model:v1", "model.pth")
     state_dict = torch.load(model_path)
@@ -104,35 +178,7 @@ def main():
         res_block=True,
     )
     model.load_state_dict(state_dict)
-
-    image = cv2.imread(image)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    angles = np.arange(0, 360, 12)
-
-    images = []
-    for angle in angles:
-        images.append(rotate(image, angle, reshape=False))
-
-    preprocessing = A.Compose([
-        A.Normalize(),
-        ToTensorV2(),
-    ])
-    images = np.stack([preprocessing(image=image)["image"] for image in images])
-    images = torch.from_numpy(images)
-
-    batch_size = 4
-    predictions = batched_prediction(model, images, batch_size)
-    predictions = [x.numpy()[0].astype(np.uint8) * 255 for x in predictions]
-    predictions = [rotate(pred, -angle, reshape=False) for pred, angle in zip(predictions, angles)]
-    predictions = [overlay(image, pred) for pred in predictions]
-
-    frames = []
-    for angle, pred in zip(angles, predictions):
-        frame = Image.fromarray(pred)
-        draw = ImageDraw.Draw(frame)
-        draw.text((0, 0), f"Angle: {angle}", (255, 255, 255))
-        frames.append(frame)
-    imageio.mimsave("predictions.gif", frames, fps=8)
+    evaluate_rotation_equivariance(model, image, gt)
 
 
 if __name__ == "__main__":
